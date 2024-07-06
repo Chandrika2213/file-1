1fe659a55ccad429890109ea1d64bca30c200f01
Chandrika2213 <pasupuletiechandrika@gmail.com>
2024-07-06 16:04:51 +0530

    first commit

---
+++ username.js
@@ -0,0 +1,8 @@
+const express=require('express')
+const app=express()
+app.get('/profile/:username',function(req,res){
+
+    res.send(`the profile is of ${req.params.username}`)
+
+})
+app.listen(8082)
